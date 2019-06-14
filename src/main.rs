d7ef89649ef70a54ffe4320d64520df8611ce6c9
Reinier Maas <ReinierMaas@Hotmail.com>
2019-06-14 10:13:19 +0200

    Working on windows :D

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-#[macro_use]
-extern crate structopt;
+extern crate colored;
 extern crate regex;
-extern crate termion;
+extern crate structopt;
+extern crate walkdir;
 
 use std::fs::File;
 use std::io;
-use std::io::BufRead;
-use std::io::BufReader;
+use std::io::Read;
 use std::path::PathBuf;
 
-use structopt::StructOpt;
+use colored::*;
 use regex::Regex;
-use termion::{color, style};
+use structopt::StructOpt;
+use walkdir::WalkDir;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -21,25 +21,46 @@ struct Opt {
     file: Option<PathBuf>,
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let opt = Opt::from_args();
-    let regex = Regex::new(&opt.regex).unwrap();
-    let lines: Vec<_>;
-    if let &Some(ref path) = &opt.file {
-        lines = BufReader::new(File::open(path).unwrap())
-            .lines().flat_map(|x|x).collect();
+    let regex = Regex::new(&opt.regex).expect("Could not parse regex!");
+    let mut file_content = String::new();
+    if let Some(path) = opt.file {
+        WalkDir::new(path)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|dir_entry| dir_entry.path().is_file())
+            .for_each(|dir_entry| {
+                file_content.truncate(0);
+                if let Ok(..) = File::open(dir_entry.path())
+                    .expect("Could not open file!")
+                    .read_to_string(&mut file_content)
+                {
+                    find_match(
+                        &regex,
+                        &format!("{}", dir_entry.path().display()),
+                        &file_content,
+                    );
+                }
+            });
     } else {
         let stdin = io::stdin();
-        lines = stdin.lock().lines().flat_map(|x| x).collect();
-    };
-    for ref line in lines {
+        stdin.lock().read_to_string(&mut file_content)?;
+        find_match(&regex, "stdin", &file_content);
+    }
+    Ok(())
+}
+fn find_match(regex: &Regex, file_path: &str, file_content: &str) {
+    for (line_number, line) in file_content.lines().enumerate() {
         if let Some(mat) = regex.find(line) {
-            println!("{}{}{}{}{}"
-                , &line[..mat.start()]
-                , color::Fg(color::Red)
-                , &line[mat.start()..mat.end()]
-                , style::Reset
-                , &line[mat.end()..])
+            println!(
+                "[{}:{}:]{}{}{}",
+                file_path,
+                line_number + 1,
+                &line[..mat.start()],
+                &line[mat.start()..mat.end()].red(),
+                &line[mat.end()..]
+            )
         }
     }
 }
